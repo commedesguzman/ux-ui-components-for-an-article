15f171b4312cfe18a5512008c48d2fa07b24d2d9
Elle Jullia De Guzman <ellejulliadeguzman@Elles-MacBook-Pro.local>
2020-04-02 05:02:59 -0400

    carousel and infinite scrolling

--- ux-ui-article-master/js/index.js
+++ ux-ui-article-master/js/index.js
@@ -1,7 +1,36 @@
+////STACKED CAROUSEL//////
+
+/*
 let $photos = document.querySelector('.container')
 
 
 $photos.addEventListener('click', (event) => {
     $photos.classList.toggle('carousel_slide')
 })
+*/
 
+////INFINITE SCROLLING//////
+  
+
+let winH = document.documentElement.clientHeight
+    let docH = document.documentElement.scrollHeight
+    let winY = window.scrollY
+    let maxY = docH - winH
+  
+    if (winY >= maxY) {
+      // You have definitely hit the bottom
+  
+      // Add new content when we hit the bottom
+      document.querySelector('#carousel').innerHTML += `
+        <li class="carousel_slide">
+          
+        </li>
+        `
+    }
+  }
+  
+  
+  window.addEventListener('load', theStateOfTheInterface)
+  window.addEventListener('scroll', theStateOfTheInterface)
+  window.addEventListener('resize', theStateOfTheInterface)
+  
