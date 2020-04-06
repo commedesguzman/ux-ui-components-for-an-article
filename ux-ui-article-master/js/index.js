30a661230af7174bd0716b5436894ed1af223d42
Laura Gema Dimas <lauragemadimas@Lauras-iMac.local>
2020-04-05 21:33:40 -0400

    Moving working code to master

--- ux-ui-article-master/js/index.js
+++ ux-ui-article-master/js/index.js
@@ -1,3 +1,57 @@
+
+ ///Progress Bar///
+ const progressContainer = document.querySelector(".progress-fill");
+
+ window.addEventListener("scroll", () => {
+   //console.log("scroll down")
+ 
+   const roomscroll = document.documentElement.scrollHeight - window.innerHeight;
+ 
+   const totalscroll = window.scrollY;
+ 
+   let percentageRoomscroll = 100;
+ 
+   // if user can scroll down
+   console.log(roomscroll)
+   if (roomscroll > 0) {
+     percentageRoomscroll = Math.ceil(totalscroll / roomscroll * 100);
+   }
+ 
+ 
+   progressContainer.style.width = `${percentageRoomscroll}%`;
+ }); 
+
+
+
+ //SCROLLSPY TO HIGHLIGHT NAV LINKS WHEN SCROLLING
+
+ (function() {
+  'use strict';
+
+    var section = document.querySelectorAll(".container");
+    var section = document.querySelectorAll('.content');
+    var sections = {};
+    var i = 0;
+
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop;
+    });
+ window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    for (i in sections) {
+      if (sections[i] <= scrollPosition + 160) {
+        document.querySelector('.active').setAttribute('class', '');
+        console.log(i);
+        debugger;
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
+
+
+/*
+////CARROUSEL////
 rollBtn.addEventListener (`click`, (event) => {
 
   document.querySelector(`.carousel`).style.display = ("block");
@@ -54,37 +108,7 @@ let winH = document.documentElement.clientHeight
   window.addEventListener('load', theStateOfTheInterface)
   window.addEventListener('scroll', theStateOfTheInterface)
   window.addEventListener('resize', theStateOfTheInterface)
-  
-
-  
- ///Progress Bar///
- const progressContainer = document.querySelector(".progress-fill");
-
- window.addEventListener("scroll", () => {
-   //console.log("scroll down")
- 
-   const roomscroll = document.documentElement.scrollHeight - window.innerHeight;
- 
-   const totalscroll = window.scrollY;
- 
-   let percentageRoomscroll = 100;
- 
-   // if user can scroll down
-   console.log(roomscroll)
-   if (roomscroll > 0) {
-     percentageRoomscroll = Math.ceil(totalscroll / roomscroll * 100);
-   }
- 
- 
-   progressContainer.style.width = `${percentageRoomscroll}%`;
- }); 
-
-
-
- //SCROLLSPY TO HIGHLIGHT NAV LINKS WHEN SCROLLING
-
- 
-
+  */
 
 
 
